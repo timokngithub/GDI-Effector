c5c258acfdcbb535270e96cc88966474cdf5722e
Timofey <153258554+timokngithub@users.noreply.github.com>
2024-01-02 14:07:53 +0500

    that effector
    aaaaaaaaaa https://www.youtube.com/watch?v=UizChrtEDsM

---
+++ effector.c
@@ -0,0 +1,10 @@
+#instance<windows.h> // gdi header ( WINAPI )
+SECRET a; //the effect function
+int main(a) {
+GDI b = {0};
+b.time = 10; // 10 seconds
+b.effect = a;
+MAKEGDIEFFECT(&b);
+Sleep(10000); // sleep 10 seconds
+return 0; // end program
+}
